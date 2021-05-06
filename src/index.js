0b20a18546b97038b91af6a521db11d616b32cda
Martin Bonnin <martin@mbonnin.net>
2021-05-06 10:48:25 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,59 @@
+    import ws from 'ws'; // yarn add ws
+    import { useServer } from 'graphql-ws/lib/use/ws';
+    import { buildSchema } from 'graphql';
+
+    // Construct a schema, using GraphQL schema language
+    const schema = buildSchema(`
+    type Query {
+        hello: String
+    }
+    type Subscription {
+        greetings: String
+    }
+    `);
+
+    // The roots provide resolvers for each GraphQL operation
+    const roots = {
+    query: {
+        hello: () => 'Hello World!',
+    },
+    subscription: {
+        greetings: function* sayHiIn5Languages() {
+        for (const hi of ['Hi', 'Bonjour', 'Hola', 'Ciao', 'Zdravo']) {
+            yield { greetings: hi };
+        }
+        },
+    },
+    };
+
+    let port = 9090;
+    const server = new ws.Server({
+    port: port,
+    path: '/graphql',
+    });
+
+    useServer(
+        {
+          schema,
+          roots,
+          onConnect: (ctx) => {
+            console.log('Connect', ctx);
+          },
+          onSubscribe: (ctx, msg) => {
+            console.log('Subscribe', { ctx, msg });
+          },
+          onNext: (ctx, msg, args, result) => {
+            console.debug('Next', { ctx, msg, args, result });
+          },
+          onError: (ctx, msg, errors) => {
+            console.error('Error', { ctx, msg, errors });
+          },
+          onComplete: (ctx, msg) => {
+            console.log('Complete', { ctx, msg });
+          },
+        },
+        server,
+      );
+      
+
+    console.log('Listening to port ' + port);
